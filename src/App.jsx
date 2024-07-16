295eb476c4f9ab7836d5728739a1a6875d9d62c9
axelerant-hardik <hardik.pandya@axelerant.com>
2024-07-16 18:46:21 +0530

    Create div with id and class

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+// Create a div with class large and id largeDiv with text Hi.
 function App() {
-  return "Hello World"
+  return (
+    <div id="largeDiv" className="large">
+      Hi
+    </div>
+  )
 }
 
 export default App
