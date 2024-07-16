ceaaccf7b80881e20f191630371b29d444fc0833
axelerant-hardik <hardik.pandya@axelerant.com>
2024-07-16 22:35:48 +0530

    Create component with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+// Create a TodoListItem component.
+// Props: children = name; isComplete: boolean
+// Checkbox: checked if complete
+// Label (children value)
+
+import { TodoListItem } from "./TodoListItem"
+
 function App() {
-  return "Hello World"
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        <TodoListItem isComplete={true}>Hardik Pandya</TodoListItem>
+        <TodoListItem isComplete={false}>Virat Kohli</TodoListItem>
+        <TodoListItem isComplete={true}>Rohit Sharma</TodoListItem>
+      </ul>
+    </div>
+  )
 }
 
 export default App
